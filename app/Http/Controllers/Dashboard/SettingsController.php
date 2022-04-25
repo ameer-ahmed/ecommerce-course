b7674d4a66618563da23799a35c206d2a54c52e3
Ameer Ahmed <ameer2001an@gmail.com>
2022-04-26 01:42:11 +0200

    end 'shipping' branch

--- app/Http/Controllers/Dashboard/SettingsController.php
+++ app/Http/Controllers/Dashboard/SettingsController.php
@@ -3,8 +3,10 @@
 namespace EcommerceCourse\Http\Controllers\Dashboard;
 
 use EcommerceCourse\Http\Controllers\Controller;
+use EcommerceCourse\Http\Requests\ShippingMethodsRequest;
 use EcommerceCourse\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SettingsController extends Controller
 {
@@ -15,11 +17,25 @@ class SettingsController extends Controller
             $key = $type . '_shipping';
             $shippingType = Setting::where('key', $key)->first();
             return view('admin.settings.shipping.edit', compact('shippingType'));
+//            return $shippingType;
         }
         return redirect()->route('admin.home');
     }
 
-    public function saveShipping(Request $request, $type) {
-
+    public function saveShipping(ShippingMethodsRequest $request, $id) {
+        try {
+            $shippingMethod = Setting::find($id);
+            DB::beginTransaction();
+            $shippingMethod->update([
+                'plain_value' => $request->plain_value,
+            ]);
+            $shippingMethod->value = $request->value;
+            $shippingMethod->save();
+            DB::commit();
+            return redirect()->back()->with(['success' => 'تم التحديث بنجاح.']);
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return redirect()->back()->with(['error' => 'حدث خطأ أثناء حفظ التعديلات.']);
+        }
     }
 }
